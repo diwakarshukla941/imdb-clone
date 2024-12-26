b6c7a883284e950aba76b5a303721c4de399e36d
diwakarshukla941 <shukladiwakar941@gmail.com>
2024-12-26 18:34:24 +0530

    imdb clone

---
+++ src/components/Movies.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Movies() {
+  return (
+    <div>
+      <h1>this is movie components</h1>
+    </div>
+  )
+}
+
+export default Movies
---
+++ src/components/WatchList.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function WatchList() {
+  return (
+    <div>
+      <h1>this is watchList</h1>
+    </div>
+  )
+}
+
+export default WatchList
